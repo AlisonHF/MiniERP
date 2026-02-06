529b364fab9347ae3db93a2cf04ff8266d2d52a5
Alison Faria <Alison>
2026-02-05 21:30:14 -0300

    Inclusão de carregamento dinâmico de arquivos css/js pelo template

--- application/core/MY_Loader.php
+++ application/core/MY_Loader.php
@@ -10,7 +10,6 @@ class MY_Loader extends CI_Loader
         // Caso houver alguma view que não utilizara o template padrão, colocar na lista
         $withoutTemplate = [
             'template/template',
-            
         ];
 
         if (in_array($view, $withoutTemplate)) {
@@ -23,9 +22,41 @@ class MY_Loader extends CI_Loader
 
         $content = parent::view($view, $vars, TRUE);
 
+        $assets = $this->detectAssets($view);
+
         return parent::view('template/template', [
-            'content' => $content
+            'content' => $content,
+            'css' => $assets['css'],
+            'js'  => $assets['js'],
         ], $return);
+    }
+
+    /**
+     * Summary of detectAssets
+     * @param string $view
+     * @return array
+     * 
+     * Padrão de criação de arquivos assets
+     * assets/css/modulo/view.css
+     * assets/js/modulo/view.js
+     */
+    private function detectAssets(string $view): array
+    {
+        $css = [];
+        $js  = [];
+
+        $cssPath = FCPATH . "assets/css/{$view}.css";
+        $jsPath  = FCPATH . "assets/js/{$view}.js";
+
+        if (file_exists($cssPath)) {
+            $css[] = 'css/' . $view;
+        }
+
+        if (file_exists($jsPath)) {
+            $js[] = 'js/' . $view;
+        }
+
+        return compact('css', 'js');
     }
 
     private function isAjax(): bool
